79c1c9d5dfe585268db431ddf75e19f0f6c6a238
raejin <raejin0@naver.com>
2022-04-11 17:01:48 +0900

    Initial commit

--- src/main/java/jpabook/jpashop/domain/Delivery.java
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -1,6 +1,5 @@
 package jpabook.jpashop.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -16,7 +15,6 @@ public class Delivery {
 	@Column(name="delivery_id")
 	private Long id;
 
-	@JsonIgnore
 	@OneToOne(mappedBy="delivery", fetch = LAZY)
 	private Order order;
 
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,11 +1,9 @@
 package jpabook.jpashop.domain;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,13 +15,11 @@ public class Member {
 	@Column(name="member_id")
 	private Long id;
 
-	@NotEmpty
 	private String name;
 
 	@Embedded  // 클래스 선언 쪽에 Embeddable이나 사용하는 곳에 @Embedded 둘 중 하나만 해줘도 무방하다.
 	private Address address;
 
-	@JsonIgnore
 	@OneToMany(mappedBy = "member")
 	private List<Order> orders = new ArrayList<>();
 
