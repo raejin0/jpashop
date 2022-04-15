c6bb135d5bdf6f8eb7001bdae5e9aad8b4f71157
raejin <raejin0@naver.com>
2022-04-15 14:03:56 +0900

    lazy loading and searching performance optimization

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,13 +1,26 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		Hibernate5Module hibernate5Module = new Hibernate5Module();
+
+		//hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true); // lazy loading 무시하고 다 가져옴
+
+		return hibernate5Module;
+
+
 	}
 
 }
